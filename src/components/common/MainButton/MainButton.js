742ac434305d67f16faf98d2fbb9858bdc6d13f3
James Plummer <plum_1990@hotmail.co.uk>
2021-01-31 10:34:31 +0000

    create main button component

---
+++ src/components/common/MainButton/MainButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Pressable, Text } from "react-native";
+
+const MainButton = ({ width, colorBG, colorText, label, ripple }) => {
+  return (
+    <Pressable
+      style={{
+        backgroundColor: colorBG,
+        width: width,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 42,
+        marginTop: 36,
+      }}
+      android_ripple={{
+        color: ripple,
+        borderless: false,
+        radius: 150,
+      }}
+    >
+      <Text
+        style={{
+          color: colorText,
+          fontFamily: "lato-regular",
+          fontSize: 18,
+        }}
+      >
+        {label}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default MainButton;
