f51150abbbcfd290d76570b3f0497e783eb77faa
James Plummer <plum_1990@hotmail.co.uk>
2021-01-31 10:36:40 +0000

    create login screen

---
+++ src/screens/Login.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { View, TextInput, StyleSheet, StatusBar } from "react-native";
+
+import base from "../styles/base";
+
+import Header from "../components/common/Header/Header";
+import MainButton from "../components/common/MainButton/MainButton";
+import TextButton from "../components/common/TextButton/TextButton";
+import AppTitle from "../components/authUI/AppTitle/AppTitle";
+import SignupButton from "../components/authUI/SignupButton/SignupButton";
+
+const { colors } = base;
+const { mainInput } = base;
+
+const Login = ({ navigation }) => {
+  return (
+    <View style={styles.screen}>
+      <Header statusbar="light" />
+      <View style={styles.contentWrapper}>
+        <AppTitle
+          title1="chrono"
+          title2="focus"
+          subtitle="Optimise your time"
+        />
+        <View style={styles.inputBox}>
+          <TextInput placeholder="Email" style={mainInput}></TextInput>
+          <TextInput placeholder="Password" style={mainInput}></TextInput>
+          <MainButton
+            label="Log in"
+            width="50%"
+            colorBG={colors.buttonPrimary}
+            colorText={colors.buttonText}
+            ripple={colors.buttonPrimaryRipple}
+          />
+          <TextButton
+            onPress={() => navigation.navigate("Forgotten Password")}
+            label="Forgotten password?"
+            color={colors.textSecondary}
+            ripple={colors.buttonTextRipple}
+          />
+        </View>
+        <SignupButton
+          onPress={() => navigation.navigate("Sign Up")}
+          labelColor={colors.textPrimary}
+          buttonColor={colors.buttonSecondary}
+          ripple={colors.buttonSecondaryRipple}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  contentWrapper: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  inputBox: { alignItems: "center", width: "100%" },
+});
