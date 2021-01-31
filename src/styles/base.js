5aa56d0729a1bf0be03557d055d8d73bb3c92d20
James Plummer <plum_1990@hotmail.co.uk>
2021-01-31 10:36:25 +0000

    create base global styles

---
+++ src/styles/base.js
@@ -0,0 +1,28 @@
+const base = {
+  colors: {
+    menuPrimary: "#818CA0",
+    menuSecondary: "#454851",
+    buttonPrimary: "#3DB6F9",
+    buttonSecondary: "#5484CB",
+    buttonText: "#F5F5F5",
+    buttonPrimaryRipple: "#99D9FD",
+    buttonSecondaryRipple: "#B5D2FD",
+    buttonTextRipple: "#EBEBEB",
+    textPrimary: "#454851",
+    textSecondary: "#818CA0",
+    headingPrimary: "#705E57",
+    headingSecondary: "#EBEBEB",
+  },
+  mainInput: {
+    backgroundColor: "#EBEBEB",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 6,
+    width: "70%",
+    fontSize: 18,
+    fontFamily: "lato-regular",
+    color: "#454851",
+  },
+};
+
+export default base;
