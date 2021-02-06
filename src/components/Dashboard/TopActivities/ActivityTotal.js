c4afe1a3826d58b32f0303a81c4b335333fbf991
James Plummer <plum_1990@hotmail.co.uk>
2021-02-06 15:02:42 +0000

    create dashboard activities totals

---
+++ src/components/Dashboard/TopActivities/ActivityTotal.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { color } from "react-native-reanimated";
+
+import base from "../../../styles/base";
+const { colors } = base;
+
+// basic summary of activity containing title, color and total hours
+const ActivityTotal = ({ color, title, total }) => {
+  let renderTitle;
+  if (title.length > 10) {
+    renderTitle = `${title.substring(0, 7)}...`;
+  } else {
+    renderTitle = title;
+  }
+  return (
+    <View style={styles.container}>
+      <View style={{ ...styles.color, backgroundColor: color }}></View>
+      <Text style={styles.title}>{renderTitle}</Text>
+      <Text style={styles.total}>[ {total} hours ]</Text>
+    </View>
+  );
+};
+
+export default ActivityTotal;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginVertical: 4,
+  },
+  color: {
+    width: 22,
+    height: 22,
+  },
+  title: {
+    textTransform: "capitalize",
+    width: 80,
+    marginHorizontal: 10,
+    color: colors.textPrimary,
+  },
+  total: {
+    color: colors.headingPrimary,
+  },
+});
