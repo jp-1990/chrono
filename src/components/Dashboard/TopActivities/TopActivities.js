16995844dd6ad540ba03b26ad4f10727ef7c3528
James Plummer <plum_1990@hotmail.co.uk>
2021-02-06 15:02:24 +0000

    create dashboard top activities

---
+++ src/components/Dashboard/TopActivities/TopActivities.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import ActivityTotal from "./ActivityTotal";
+
+const TopActivities = ({ activities }) => {
+  // activities contains array of items with color, title and total
+
+  // enforce only 3 items to be rendered
+  while (activities.length > 3) {
+    activities.pop();
+  }
+
+  // build array to be rendered
+  const renderedActivities = activities.map((e, i) => {
+    return (
+      <ActivityTotal key={i} color={e.color} title={e.title} total={e.total} />
+    );
+  });
+
+  return <View>{renderedActivities}</View>;
+};
+
+export default TopActivities;
