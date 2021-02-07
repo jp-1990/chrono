ece554cd2aafbd1f26ae639d1d6bebb6bdba3c57
James Plummer <plum_1990@hotmail.co.uk>
2021-02-07 14:10:00 +0000

    create timeline key

---
+++ src/components/Timeline/ActivitiesKey/ActivitiesKey.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import ActivityTotal from "../../common/ActivityTotal/ActivityTotal";
+
+const ActivitiesKey = ({ activities }) => {
+  // activities contains array of items with color, title and total
+
+  // build arrays to be rendered
+  const leftCol = [];
+  const rightCol = [];
+
+  for (let i = 0, j = activities.length; i < j; i++) {
+    if (i % 2 === 0)
+      leftCol.push(
+        <ActivityTotal
+          key={i}
+          color={activities[i].color}
+          title={activities[i].title}
+          total={activities[i].total}
+          totalVisible={false}
+        />
+      );
+    if (i % 2 !== 0)
+      rightCol.push(
+        <ActivityTotal
+          key={i}
+          color={activities[i].color}
+          title={activities[i].title}
+          total={activities[i].total}
+          totalVisible={false}
+        />
+      );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View>{leftCol}</View>
+      <View>{rightCol}</View>
+    </View>
+  );
+};
+
+export default ActivitiesKey;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+});
