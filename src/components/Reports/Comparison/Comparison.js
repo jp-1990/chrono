dfab66d1fdac669f7a7c231819a3a0de226e11a9
James Plummer <plum_1990@hotmail.co.uk>
2021-02-08 12:25:54 +0000

    create comparison summary component

---
+++ src/components/Reports/Comparison/Comparison.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import Subtitle from "../../common/Subtitle/Subtitle";
+import ActivityTotal from "../../common/ActivityTotal/ActivityTotal";
+
+import base from "../../../styles/base";
+const { colors } = base;
+
+const Comparison = ({ activities }) => {
+  // activities contains array of items with color, title and total
+
+  // activity items to render
+  const renderedItems = [];
+  for (let i = 0, j = activities.length; i < j; i++) {
+    // placeholder value
+    const temp = [3, 2, 5, 8, 1, 4, 6, 3, 12, 9];
+    const percentage = i % 2 === 0 ? `+ ${temp[i]}%` : `- ${temp[i]}%`;
+    const value =
+      i % 2 === 0 ? `[ + ${temp[i]} hours ]` : `[ - ${temp[i]} hours ]`;
+    const tempstyle =
+      i % 2 === 0 ? { color: colors.positive } : { color: colors.negative };
+
+    renderedItems.push(
+      <View key={i} style={styles.row}>
+        <ActivityTotal
+          color={activities[i].color}
+          title={activities[i].title}
+          total={activities[i].total}
+          totalVisible={false}
+        />
+        <View style={styles.percentageContainer}>
+          <Text style={{ ...styles.percentageText, ...tempstyle }}>
+            {percentage}
+          </Text>
+          <Text style={styles.valueText}>{value}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <View style={styles.titles}>
+        <Subtitle text="comparison" />
+        <View style={styles.dateContainer}>
+          <View style={styles.date}>
+            <Text style={styles.month}>jan</Text>
+            <Text style={styles.year}> 2021</Text>
+          </View>
+          <Text style={styles.vs}>vs</Text>
+          <View style={styles.date}>
+            <Text style={styles.month}>dec</Text>
+            <Text style={styles.year}> 2020</Text>
+          </View>
+        </View>
+      </View>
+      {renderedItems}
+    </View>
+  );
+};
+
+export default Comparison;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  percentageContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "50%",
+  },
+  percentageText: {
+    width: 45,
+  },
+  valueText: {
+    color: colors.headingPrimary,
+    fontFamily: "lato-light",
+  },
+  titles: {
+    marginVertical: 15,
+  },
+  dateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  date: {
+    flexDirection: "row",
+  },
+  month: {
+    textTransform: "uppercase",
+    color: colors.headingPrimary,
+    fontFamily: "lato-bold",
+    fontSize: 16,
+  },
+  year: {
+    color: colors.headingSecondary,
+    fontFamily: "lato-light",
+    fontSize: 16,
+  },
+  vs: {
+    color: colors.headingSecondary,
+    marginHorizontal: 8,
+  },
+});
