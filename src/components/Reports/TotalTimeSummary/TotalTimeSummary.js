6f77f862c3617549e79b42025df9362ec5eea4d1
James Plummer <plum_1990@hotmail.co.uk>
2021-02-08 12:25:35 +0000

    create total time summary component

---
+++ src/components/Reports/TotalTimeSummary/TotalTimeSummary.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import base from "../../../styles/base";
+const { colors } = base;
+
+const TotalTimeSummary = ({ title, time }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.timeContainer}>
+        <Text style={styles.time}>{time}</Text>
+        <Text style={styles.hours}> hours</Text>
+      </View>
+    </View>
+  );
+};
+
+export default TotalTimeSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+    marginVertical: 3,
+  },
+  title: {
+    fontSize: 20,
+    color: colors.headingSecondary,
+    fontFamily: "lato-light",
+  },
+  timeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  time: {
+    fontSize: 24,
+    color: colors.headingSecondary,
+    fontFamily: "lato-light",
+  },
+  hours: {
+    fontSize: 18,
+    color: colors.headingSecondary,
+    fontFamily: "lato-light",
+  },
+});
