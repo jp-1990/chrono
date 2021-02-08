e8057dad0d5c4194b24a2f1673d048411a93ffe7
James Plummer <plum_1990@hotmail.co.uk>
2021-02-08 12:27:37 +0000

    update modified to use scrollview and increase bar width

--- src/components/Reports/BarChart/BarChart.js
+++ src/components/Reports/BarChart/BarChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import ChartColumn from "./ChartColumn/ChartColumn";
 
@@ -12,32 +12,38 @@ const BarChart = ({ data }) => {
   const chartColumns = [];
   for (let i = 0, j = data.length; i < j; i++) {
     chartColumns.push(
-      <ChartColumn key={i} width={width} data={data[i][i + 1]} />
+      <ChartColumn
+        key={i}
+        width={18}
+        date={i + 1}
+        max={data.length}
+        data={data[i][i + 1]}
+      />
     );
   }
 
   return (
     <View style={styles.chartContainer}>
       <View style={styles.top}>
-        <View style={styles.chart}>{chartColumns}</View>
+        <View style={styles.chart}>
+          <ScrollView
+            horizontal={true}
+            style={{ height: "auto" }}
+            showsHorizontalScrollIndicator={false}
+          >
+            {chartColumns}
+          </ScrollView>
+        </View>
+        <View style={styles.yAxisLine}></View>
         <View style={styles.yAxis}>
           <Text style={styles.labels}>12</Text>
           <Text style={styles.labels}>8</Text>
           <Text style={styles.labels}>4</Text>
-          <Text style={styles.labels}> </Text>
+          <Text style={styles.labels}>0</Text>
         </View>
       </View>
-      <View style={styles.bottom}>
-        <View style={styles.xAxis}>
-          <View style={styles.xAxisText}>
-            <Text style={styles.labels}>1</Text>
-            <Text style={styles.labels}>jan</Text>
-          </View>
-          <View style={styles.xAxisText}>
-            <Text style={styles.labels}>31</Text>
-            <Text style={styles.labels}>jan</Text>
-          </View>
-        </View>
+      <View style={styles.xAxis}>
+        <Text style={styles.xAxisLabel}>january</Text>
       </View>
     </View>
   );
@@ -47,16 +53,12 @@ export default BarChart;
 
 const styles = StyleSheet.create({
   chartContainer: { width: "100%", height: 250, marginVertical: 20 },
-  top: { flexDirection: "row", width: "100%", height: "85%" },
+  top: { flexDirection: "row", width: "100%", height: "90%" },
   chart: {
     width: "91%",
     height: "97%",
     marginLeft: "4%",
     marginTop: "3%",
-    borderRightColor: colors.headingSecondary,
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.headingSecondary,
     flexDirection: "row",
     alignItems: "flex-end",
     paddingHorizontal: 1,
@@ -70,14 +72,22 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginLeft: "1%",
+    paddingBottom: 12,
   },
-  bottom: {},
+  yAxisLine: {
+    borderLeftColor: colors.headingSecondary,
+    borderLeftWidth: 1,
+    marginBottom: 22,
+  },
   xAxis: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: "1%",
-  },
-  xAxisText: {
+    height: "10%",
+    marginTop: 8,
     alignItems: "center",
+  },
+  xAxisLabel: {
+    textTransform: "capitalize",
+    color: colors.headingSecondary,
+    fontFamily: "lato-light",
+    fontSize: 22,
   },
 });
--- src/components/Reports/BarChart/ChartColumn/ChartColumn.js
+++ src/components/Reports/BarChart/ChartColumn/ChartColumn.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import ChartBar from "./ChartBar";
 
-const ChartColumn = ({ data, width }) => {
+import base from "../../../../styles/base";
+const { colors } = base;
+
+const ChartColumn = ({ data, width, date, max }) => {
   const column = data.map((e, i) => {
     return <ChartBar key={i} color={e.color} height={e.height} />;
   });
 
+  // date display logic (seperate later?)
+  let displayDate = null;
+  if (date === 1) displayDate = 1;
+  if (date === max) displayDate = max;
+  if ((date - 1) % 6 === 0) displayDate = date;
+
   return (
-    <View style={{ ...styles.columnContainer, width: `${width}%` }}>
-      {column}
-    </View>
+    <>
+      <View style={{ ...styles.columnContainer, width: width }}>
+        {column}
+        <View style={styles.line}></View>
+        <View style={styles.center}>
+          <Text style={styles.label}>{displayDate}</Text>
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -22,5 +37,20 @@ const styles = StyleSheet.create({
     paddingLeft: 1,
     height: "100%",
     justifyContent: "flex-end",
+    position: "relative",
+  },
+  line: {
+    marginTop: 1,
+    width: "110%",
+    borderTopColor: colors.headingSecondary,
+    borderTopWidth: 1,
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    color: colors.headingSecondary,
+    marginTop: 4,
   },
 });
