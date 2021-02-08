528465f60af29ae1c4179d25a5849a7f2b9964c2
James Plummer <plum_1990@hotmail.co.uk>
2021-02-08 12:24:17 +0000

    update added icon arrows

--- src/components/common/MonthSelector/MonthSelector.js
+++ src/components/common/MonthSelector/MonthSelector.js
@@ -11,16 +11,26 @@ const MonthSelector = ({ Left, Right }) => {
       <View style={styles.container}>
         <MaterialIcons
           name="arrow-left"
-          size={30}
+          size={28}
           color={colors.headingSecondary}
         />
         <View style={styles.dateBox}>{Left}</View>
+        <MaterialIcons
+          name="arrow-right"
+          size={28}
+          color={colors.headingSecondary}
+        />
       </View>
       <View style={styles.container}>
+        <MaterialIcons
+          name="arrow-left"
+          size={28}
+          color={colors.headingSecondary}
+        />
         <View style={styles.dateBox}>{Right}</View>
         <MaterialIcons
           name="arrow-right"
-          size={30}
+          size={28}
           color={colors.headingSecondary}
         />
       </View>
@@ -32,13 +42,14 @@ export default MonthSelector;
 
 const styles = StyleSheet.create({
   wrapper: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "flex-end",
     width: 208,
   },
   container: {
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "flex-end",
   },
   dateBox: {
     height: 24,
