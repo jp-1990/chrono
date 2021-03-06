dbdc0cece63a24861efd921adb9afcf6884c91a2
James Plummer <plum_1990@hotmail.co.uk>
2021-03-06 16:37:40 +0000

    create global types for data array

---
+++ src/types/data.ts
@@ -0,0 +1,9 @@
+export type DataTypes = {
+  color: string;
+  width: string;
+  margin: string | undefined;
+};
+
+export type DataArray = {
+  data: { color: string; width: string; margin: string | undefined }[];
+};
