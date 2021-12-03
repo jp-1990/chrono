a1c376060dd890c40d6fe4c5927fcce42096ba98
James Plummer <plum_1990@hotmail.co.uk>
2021-12-03 23:23:55 +0000

    [create] update task mutations

--- src/graphql/tasks/mutations/index.ts
+++ src/graphql/tasks/mutations/index.ts
@@ -3,3 +3,14 @@ export type {
   CreateTaskMutationArgs,
   CreateTaskMutationRes,
 } from "./createTask";
+
+export {
+  UpdateTaskMutation,
+  UpdateTasksColourAndGroupMutation,
+} from "./updateTask";
+export type {
+  UpdateTaskMutationArgs,
+  UpdateTaskMutationRes,
+  UpdateTasksColourAndGroupArgs,
+  UpdateTasksColourAndGroupRes,
+} from "./updateTask";
---
+++ src/graphql/tasks/mutations/updateTask.ts
@@ -0,0 +1,72 @@
+import { gql } from "@apollo/client";
+import { TaskAPIResponse } from "../../../types";
+
+export const UpdateTaskMutation = gql`
+  mutation updateTask(
+    $id: String!
+    $title: String
+    $notes: String
+    $start: String
+    $end: String
+  ) {
+    updateTask(
+      id: $id
+      title: $title
+      description: $notes
+      start: $start
+      end: $end
+    ) {
+      id
+      title
+      description
+      start
+      end
+      percentageTimes
+    }
+  }
+`;
+export interface UpdateTaskMutationArgs {
+  id: string;
+  title?: string;
+  notes?: string;
+  start?: string;
+  end?: string;
+}
+export interface UpdateTaskMutationRes {
+  updateTask: Omit<
+    TaskAPIResponse,
+    "user" | "luminance" | "createdAt" | "colour" | "group"
+  >;
+}
+
+export const UpdateTasksColourAndGroupMutation = gql`
+  mutation updateTaskColourAndGroup(
+    $title: String!
+    $colour: String
+    $activity: String
+  ) {
+    updateTaskColourAndGroup(title: $title, group: $activity, colour: $colour) {
+      id
+      group
+      colour
+      luminance
+    }
+  }
+`;
+export interface UpdateTasksColourAndGroupArgs {
+  title: string;
+  activity?: string;
+  colour?: string;
+}
+export interface UpdateTasksColourAndGroupRes {
+  updateTaskColourAndGroup: Omit<
+    TaskAPIResponse,
+    | "user"
+    | "percentageTimes"
+    | "createdAt"
+    | "start"
+    | "end"
+    | "description"
+    | "title"
+  >[];
+}
