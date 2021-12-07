df34322aac58fb247eba6e7570a3ffbf46d7fcf3
James Plummer <plum_1990@hotmail.co.uk>
2021-12-07 22:35:40 +0000

    [update] renabled navigation and added an indication of which nav item is currently in focus

--- src/components/common/BottomNav/BottomNav.tsx
+++ src/components/common/BottomNav/BottomNav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Pressable } from 'react-native';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { useCreateUpdateModalContext } from '../../../Providers';
 import { FAB } from '../FAB';
@@ -11,31 +11,66 @@ const BottomNav: React.FC = () => {
   const { actions } = useCreateUpdateModalContext();
   // useNavigation hook works with react context in the background and provides the navigation object from the current screen
   const navigation = useNavigation();
+  const route = useRoute();
+
   // useRoute provides route object from current screen
 
   return (
     <View style={styles.container}>
       <Pressable
         onPress={() => navigation.navigate('Dashboard')}
-        style={styles.iconBox}
+        style={({ pressed }) => [
+          styles.iconBox,
+          pressed ? styles.pressed : null,
+        ]}
       >
-        <MaterialCommunityIcons name="view-dashboard" size={28} color="white" />
+        <MaterialCommunityIcons
+          name="view-dashboard"
+          size={28}
+          color={route.name === 'Dashboard' ? 'white' : colors.accentPrimary}
+        />
       </Pressable>
-      <Pressable onPress={() => null} style={styles.iconBox}>
+      <Pressable
+        onPress={() => navigation.navigate('Timeline')}
+        style={({ pressed }) => [
+          styles.iconBox,
+          pressed ? styles.pressed : null,
+        ]}
+      >
         <MaterialCommunityIcons
           name="timeline-clock-outline"
           size={28}
-          color="white"
+          color={route.name === 'Timeline' ? 'white' : colors.accentPrimary}
         />
       </Pressable>
       <View style={styles.fabContainer}>
         <FAB onPress={actions.openCreateModal} />
       </View>
-      <Pressable onPress={() => null} style={styles.iconBox}>
-        <Ionicons name="stats-chart-sharp" size={28} color="white" />
+      <Pressable
+        onPress={() => navigation.navigate('Statistics')}
+        style={({ pressed }) => [
+          styles.iconBox,
+          pressed ? styles.pressed : null,
+        ]}
+      >
+        <Ionicons
+          name="stats-chart-sharp"
+          size={28}
+          color={route.name === 'Statistics' ? 'white' : colors.accentPrimary}
+        />
       </Pressable>
-      <Pressable onPress={() => null} style={styles.iconBox}>
-        <Ionicons name="person" size={28} color="white" />
+      <Pressable
+        onPress={() => null}
+        style={({ pressed }) => [
+          styles.iconBox,
+          pressed ? styles.pressed : null,
+        ]}
+      >
+        <Ionicons
+          name="person"
+          size={28}
+          color={route.name === 'Profile' ? 'white' : colors.accentPrimary}
+        />
       </Pressable>
     </View>
   );
@@ -65,4 +100,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 3,
   },
+  pressed: {
+    backgroundColor: colors.buttonTextRipple,
+  },
+  selected: {},
 });
